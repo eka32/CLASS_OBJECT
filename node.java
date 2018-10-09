2031482fcd3759a21e630f52b1094b8c43e23790
eka32 <43999320+eka32@users.noreply.github.com>
2018-10-10 00:17:28 +0800

    Create node.java

---
+++ node.java
@@ -0,0 +1,27 @@
+
+public class node {
+	//Attributes
+	String Label;
+	int value;
+	
+	//ini method getter
+	public String getLabel()
+	{
+		return Label;
+	}
+
+	public int getvalue()
+	{
+		return value;
+	}
+	//ini method setter
+	public void setLabel(String Label)
+	{
+		this.Label = Label;
+	}
+	public void setvalue(int Label)
+	{
+		this.value = value;
+	}
+}
+
