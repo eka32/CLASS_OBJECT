f5397c540cc410438c542a0c44142dc4bb2e21bb
eka32 <43999320+eka32@users.noreply.github.com>
2018-10-10 00:15:19 +0800

    Create mahasiswa.java

---
+++ mahasiswa.java
@@ -0,0 +1,75 @@
+
+public class mahasiswa {
+	
+//attributes
+	String Nama;
+	String Nim;
+	String Alamat;
+	String GologanDarah;
+	String Status;
+	int TinggiBadan;
+	int BeratBadan;
+	
+	//ini method getter
+	public String getNama()
+	{
+		return Nama;
+	}
+		public String getNim()
+	{
+		return Nim;
+	}
+		public String getAlamat()
+	{
+		return Alamat;
+	}
+		public String getGolonganDarah()
+	{
+		return getGolonganDarah();
+	}
+		public String getStatus()
+	{
+		return Status;
+	}
+		public int getTinggiBadan()
+	{
+		return TinggiBadan;
+	}
+		public int getBeratBadan()
+	{
+		return BeratBadan;
+	}
+		
+	//ini method setter
+	public void setNama(String Nama)
+	{
+		this.Nama = Nama;
+	}
+	public void setNim(String Nim)
+	{
+		this.Nim = Nim;
+	}
+	public void setAlamat(String Alamat)
+	{
+		this.Alamat = Alamat;
+	}
+	public void setGolonganDarah(String GolonganDarah)
+	{
+		this.GologanDarah = GolonganDarah;
+	}
+	public void setStatus(String Status)
+	{
+		this.Status = Status;
+	}
+	public void setTinggiBadan(int TinggiBadan)
+	{
+		this.TinggiBadan = TinggiBadan;
+	}
+		public void setBeratBadan(int BeratBadan)
+	{
+		this.Status = Status;
+	}
+}
+
+	
+	
